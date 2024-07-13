480c52733ee0cfecb3e1470e359e832ff3660966
Luan Miqueias <luanmiqueias92@gmail.com>
2024-07-13 11:53:32 -0300

    feat: added test e2e - profile, register and authenticate

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,42 @@
+import "dotenv/config";
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+import { execSync } from "child_process";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+	if (!process.env.DATABASE_URL) {
+		throw new Error("Please provide a DATABASE_URL environment variable.");
+	}
+
+	const url = new URL(process.env.DATABASE_URL);
+
+	url.searchParams.set("schema", schema);
+
+	return url.toString();
+}
 
 export default <Environment>{
 	name: "prisma",
 	transformMode: "ssr",
 
 	async setup() {
-		console.log("Setup");
+		const schema = randomUUID();
+		const newDatabaseURL = generateDatabaseURL(schema);
+
+		process.env.DATABASE_URL = newDatabaseURL;
+
+		execSync("npx prisma migrate deploy");
 
 		return {
 			async teardown() {
-				console.log("Teardown");
+				await prisma.$executeRawUnsafe(
+					`DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+				);
+
+				await prisma.$disconnect();
 			},
 		};
 	},
--- src/http/controllers/tests/profile.test.ts
+++ src/http/controllers/tests/profile.test.ts
@@ -1,3 +1,40 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
 
-test("ok", () => {});
+describe("Profile (e2e)", () => {
+	beforeAll(async () => {
+		await app.ready();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it("should be able to get user profile", async () => {
+		await request(app.server).post("/users").send({
+			name: "test",
+			email: "johndoe@example.com",
+			password: "123456",
+		});
+
+		const authResponse = await request(app.server).post("/auth/login").send({
+			email: "johndoe@example.com",
+			password: "123456",
+		});
+
+		const { token } = authResponse.body;
+
+		const response = await request(app.server)
+			.get("/me")
+			.set("Authorization", `Bearer ${token}`)
+			.send();
+
+		expect(response.statusCode).toEqual(200);
+		expect(response.body.user).toEqual(
+			expect.objectContaining({
+				email: "johndoe@example.com",
+			})
+		);
+	});
+});
