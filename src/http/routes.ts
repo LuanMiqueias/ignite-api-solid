8fbf4080932bf908683f3d7b72d2dc396ce1fec3
Luan Miqueias <luanmiqueias92@gmail.com>
2024-07-13 11:11:36 -0300

    feat: added setup for e2e test

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.controller";
-import { authenticate } from "./controllers/authenticate";
-import { profile } from "./controllers/profile";
+import { authenticate } from "./controllers/authenticate.controller";
+import { profile } from "./controllers/profile.controller";
 import { verifyJWT } from "./middlewares/verify-jwt";
 
 export const appRoutes = async (app: FastifyInstance) => {
